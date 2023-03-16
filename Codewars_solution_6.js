95a5be13d0cbb5e9d05ec4d4502625b1e49f3885
tonyiboy <antoniocodinggithub@gmail.com>
2023-03-16 22:26:38 +0100

    Codewars solution 6

---
+++ Codewars_solution_6.js
@@ -0,0 +1,9 @@
+// Task
+// Coding in function trueOrFalse, function accept 1 parameters:val, try to use 
+// the conditional statement if...else, if val value is false (val==false or it can convert to false), 
+// should return a string "false", if not, return a string "true".
+
+function trueOrFalse(val){
+    if (val)  { return 'true';}            
+    else { return 'false'; }
+  } 
