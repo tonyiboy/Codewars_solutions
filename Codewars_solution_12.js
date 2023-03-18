0ac53f84f70f4abdee581d270e1e211f61e07ac4
tonyiboy <antoniocodinggithub@gmail.com>
2023-03-18 19:03:06 +0100

    Codewars solution 12

---
+++ Codewars_solution_12.js
@@ -0,0 +1,20 @@
+//Complete function padIt, which accepts 2 parameters:
+//str: a string representing the string to pad. We will pad it with "*" 
+//symbols on its left side and on its right side.
+//n: a number indicating how many times to pad the string.
+//Behaviour
+//You need to write a loop statement within the function that loops n times. 
+//Each time through the loop it will add one * to str,
+//alternating on which side it is padded: the first time will add a * to the
+//left side of str, the second time will add a * to the right side, and so on.
+//Finally, return the padded string.
+
+function padIt(str,n){
+    let counter = 1;
+    while(counter <= n){
+      if(counter % 2 != 0) str = '*' + str;
+      if(counter % 2 == 0) str += '*'
+      counter++
+    }
+    return str
+  }
