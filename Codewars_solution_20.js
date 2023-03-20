9e0740d282e90e3eb3aedbfaef1659e7a415e23b
tonyiboy <antoniocodinggithub@gmail.com>
2023-03-20 18:42:10 +0100

    Codewars solution 20

---
+++ Codewars_solution_20.js
@@ -0,0 +1,15 @@
+// Task
+// The function pickIt accepts 1 parameter, arr, which is an array of numbers. 
+// We need to traverse arr by using a for loop. If an element is an odd number,
+// push it to the odd array, and if it's an even number, push it to the evenarray.
+// I've defined two arrays odd and even in the function, and also wrote the return statement.
+// Your work is to write a for loop.
+
+function pickIt(arr){
+    var odd=[],even=[];
+    for(var x of arr){
+    (x%2===0?even:odd).push(x)}
+    
+    
+    return [odd,even];
+  }
