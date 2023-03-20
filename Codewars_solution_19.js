5fdeac9131632559dfca65dab8ef59ae08f316f0
tonyiboy <antoniocodinggithub@gmail.com>
2023-03-20 18:40:16 +0100

    Codewars solution 19

---
+++ Codewars_solution_19.js
@@ -0,0 +1,11 @@
+// Task
+// The function pickIt accepts 1 parameter, arr, which is an array of numbers. 
+// We need to traverse arr by using a for loop. If an element is an odd number,
+// push it to the odd array, and if it's an even number, push it to the evenarray.
+// I've defined two arrays odd and even in the function, and also wrote the return statement.
+// Your work is to write a for loop.
+
+const pickIt = arr => {
+    for (let i = 0; i < 100; ++i)
+    return [arr.filter(v => v % 2 !== 0), arr.filter(v => v % 2 === 0)];
+  }
