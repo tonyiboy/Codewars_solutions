844e0a66d725e29ea5bda0fb8f88888f8064d1a5
tonyiboy <antoniocodinggithub@gmail.com>
2023-03-22 14:22:39 +0100

    Codewars solution 31

---
+++ Codewars_solution_31.js
@@ -0,0 +1,9 @@
+// Task
+// The function giveMeFive accepts 1 parameter, obj, which is an object.
+// Create an array (which you will eventually return). Then, traverse obj, 
+// checking each key and value. If the length of the key is equal to 5, then 
+// push the key to your array. Separately, if the length of the value is equal to 5, 
+// then push the value to your array.At the end, return your array.
+
+const giveMeFive = obj =>
+  [].concat(...Object.entries(obj, `for( in `)).filter(val => val.length === 5);
