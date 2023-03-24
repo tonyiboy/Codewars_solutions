e8d2e93c28e272165d25110cb55d5d8780f17091
tonyiboy <antoniocodinggithub@gmail.com>
2023-03-24 14:21:50 +0100

    Codewars solution 40

---
+++ Codewars_solution_40.js
@@ -0,0 +1,10 @@
+// Task
+// The function giveMeFive accepts 1 parameter, obj, which is an object.
+// Create an array (which you will eventually return). Then, traverse obj, 
+// checking each key and value. If the length of the key is equal to 5, then 
+// push the key to your array. Separately, if the length of the value is equal to 5, 
+// then push the value to your array.At the end, return your array.
+
+function giveMeFive(obj) {                         
+    return Object.keys(obj).reduce((arr, k) => [...arr, ...(k.length === 5 ? [k] : []), ...(obj[k].length === 5 ? [obj[k]] : [])], []);
+  }
