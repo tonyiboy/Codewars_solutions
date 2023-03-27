39b05370809febfa2b99e712233b7e3299af0441
tonyiboy <antoniocodinggithub@gmail.com>
2023-03-27 16:28:50 +0200

    Codewars solution 51

--- Codewars_solution_51.js
+++ Codewars_solution_51.js
@@ -7,3 +7,13 @@
 // "Input number is Number.NEGATIVE_INFINITY"
 // "Input number is Number.POSITIVE_INFINITY"
 // Other values should return "Input number is xxx". xxx means this number.
+
+function whatNumberIsIt(n){
+    let msg = (n===1.7976931348623157e+308) ? "Input number is Number.MAX_VALUE" :
+    (n===5e-324) ? "Input number is Number.MIN_VALUE" :
+    (isNaN(n)) ? "Input number is Number.NaN" :
+    (n===-Infinity) ? "Input number is Number.NEGATIVE_INFINITY" :
+    (n===Infinity) ? "Input number is Number.POSITIVE_INFINITY" :
+    "Input number is " + n;
+     return msg
+   }
