eb582032c5df629fdbc8c58775683f5117bd919a
tonyiboy <antoniocodinggithub@gmail.com>
2023-03-28 17:18:47 +0200

    Codewars solution 58

---
+++ Codewars_solution_58.js
@@ -0,0 +1,18 @@
+// Task
+// Coding in function whatNumberIsIt. function accept 1 parameter:n. it's a number.
+// To judge the number n. If n is one of the above five constants, return one of these string:
+// "Input number is Number.MAX_VALUE"
+// "Input number is Number.MIN_VALUE"
+// "Input number is Number.NaN"
+// "Input number is Number.NEGATIVE_INFINITY"
+// "Input number is Number.POSITIVE_INFINITY"
+// Other values should return "Input number is xxx". xxx means this number.
+
+const NUMBER_PROPERTIES = {
+    [Number.MAX_VALUE]: 'Number.MAX_VALUE',
+    [Number.MIN_VALUE]: 'Number.MIN_VALUE',
+    [-Infinity]:        'Number.NEGATIVE_INFINITY',
+    [Infinity]:         'Number.POSITIVE_INFINITY'
+  }
+  
+  const whatNumberIsIt = n => `Input number is ${isNaN(n) ? 'Number.NaN' : (NUMBER_PROPERTIES[n]||n)}`
