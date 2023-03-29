70bdef2dd0c9d00179d20509abdb62ee82fbe7c8
tonyiboy <antoniocodinggithub@gmail.com>
2023-03-30 01:47:09 +0200

    Codewars solution 67

---
+++ Codewars_solution_67.js
@@ -0,0 +1,19 @@
+// Coding in function colorOf. function accept 3 parameter:r g b. 
+// It means value of color red green and blue. the value range is 0-255.
+// Use toString(16) Convert numbers r g b to hex string form. at last, 
+// combine them to a web color code and return it.
+// the color code should starting with "#". and then use 2 characters per color.
+// for example:
+// colorOf(255,0,0) should return "#ff0000"
+// colorOf(0,111,0) should return "#006f00"
+// colorOf(1, 2 ,3) should return "#010203"
+// That's all of your work. My work is print your color code on your screen.
+
+function colorOf(r,g,b){
+    var f = function (x) {
+      return (x<16 ? '0' : '') + x.toString(16) 
+    };
+    
+    return "#" +  f(r) + f(g) + f(b);
+  }
+
