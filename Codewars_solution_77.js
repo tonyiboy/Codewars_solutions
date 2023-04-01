e16e846156bc658bd30c10341662f0257149228e
tonyiboy <antoniocodinggithub@gmail.com>
2023-04-01 17:07:16 +0200

    Codewars solution 77

---
+++ Codewars_solution_77.js
@@ -0,0 +1,19 @@
+// Coding in function howManySmaller, function accept 2 parameter:arr 
+// and n. arr is a decimal array. n is a decimal.
+// The first mission: let all elements in the array keep 
+// two decimal places(No need to convert number n).
+// The second mission: Traversal arr, count the number of the 
+// element which smaller than n and return it.
+// for example:
+// howManySmaller([1.234,1.235,1.228],1.24) should return 2
+// howManySmaller([1.1888,1.1868,1.1838],1.19) should return 1
+// howManySmaller([3.1288,3.1212,3.1205],3.1212) should return 2
+
+function howManySmaller(arr,n){
+    var r=0;
+    for(var i in arr){
+      arr[i] = parseFloat(arr[i].toFixed(2));
+      r += arr[i]<n?1:0
+    }
+    return r
+  }
