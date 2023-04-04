ce4f9c01d5796727c52a1ae8868fcf117b5be3b1
tonyiboy <antoniocodinggithub@gmail.com>
2023-04-05 00:36:08 +0200

    Codewars solution 98

---
+++ Codewars_solution_98.js
@@ -0,0 +1,23 @@
+  // Coding in function firstToLast, function accept 2 parameters:
+// str and c. str is a string. c is a char.
+// Please return the gap between the first position of c and the last position of c.
+// If there are a lot of c in the str, should return a positive integer; 
+// If there is only one c in str, should return 0; 
+// If there is no c in the str, should return -1. Retrieval should not ignored the case.
+// for example:
+// firstToLast("ababc","a") should return 2(2-0)
+// firstToLast("ababc","c") should return 0(4-4)
+// firstToLast("ababc","d") should return -1
+
+function firstToLast(str,c) {
+    first = str.indexOf(c);
+    last = str.lastIndexOf(c);
+    switch (str.split(c).length-1) {
+      case 0:
+        return -1;
+      case 1:
+        return 0;
+      default:
+        return last - first;
+    }
+  }
