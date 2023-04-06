988f36665037cb01d171ff5966f53d7b11c5b589
tonyiboy <antoniocodinggithub@gmail.com>
2023-04-07 01:06:57 +0200

    Codewars solution 110

---
+++ Codewars_solution_110.js
@@ -0,0 +1,12 @@
+// Task
+// Implement a function which accepts 2 arguments: string and separator.
+// The expected algorithm: split the string into words by spaces, split 
+// each word into separate characters and join them back with the specified 
+// separator, join all the resulting "words" back into a sentence with spaces.
+// For example:
+// splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+// splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+// splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+// splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
+
+splitAndMerge = (str,sp) => str.split(' ').reduce(({ tmp }, op) => {tmp.push(op.split('').join(sp));return { tmp }}, { tmp:[]}).tmp.join(' ')
