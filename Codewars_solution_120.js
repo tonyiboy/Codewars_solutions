d46e6027d897e4e69839c478b71fd465bf459cc7
tonyiboy <antoniocodinggithub@gmail.com>
2023-04-09 21:24:58 +0200

    Codewars solution 120

---
+++ Codewars_solution_120.js
@@ -0,0 +1,27 @@
+// Task
+// Coding in function alienLanguage, function accept 1 parameter:str. str is a sentence.
+// We translate the sentence into an alien language according to the following rules:
+// Each word in the sentence is separated by spaces. The last letter of each word in the 
+// sentence turns to lowercase, and the other letters are capitalized. Looks very strange? 
+// Because this is the form of alien language ;-)
+// for example:
+// alienLanguage("My name is John") should return "My NAMe Is JOHn"
+// alienLanguage("this is an example") should return "THIs Is An EXAMPLe"
+// alienLanguage("Hello World") should return "HELLo WORLd"
+
+function alienLanguage(str){
+    let words = str.split(" ");
+    let words1 = str.split(" ");
+    const arrOfLastLetters = [];
+    for (let i=0;i<words.length;i++){
+      words[i]=words[i].slice(-1).toLowerCase();
+      arrOfLastLetters.push(words[i]);
+    }
+    for (let i=0;i<words1.length;i++){
+      words1[i]=words1[i].toUpperCase();
+    }
+    for (let i=0;i<words.length;i++){
+      words1[i]=words1[i].slice(0,-1)+arrOfLastLetters[i];
+    }
+      return words1.join(" ");
+    }
