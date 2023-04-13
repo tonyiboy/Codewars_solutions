8b4ec6f45fa463c509123036bcbb1e6a7909a004
tonyiboy <antoniocodinggithub@gmail.com>
2023-04-13 16:42:22 +0200

    Codewars solution 136

---
+++ Codewars_solution_136.js
@@ -0,0 +1,15 @@
+// Task
+// Create a function shuffleIt. The function accepts two or more parameters. 
+// The first parameter arr is an array of numbers, followed by an arbitrary number of numeric arrays. 
+// Each numeric array contains two numbers, which are indices for elements in arr 
+// (the numbers will always be within bounds).For every such array, swap the elements. 
+// Try to use all your new skills: arrow functions, the spread operator, destructuring, and rest parameters.
+// Example:
+// shuffleIt([1,2,3,4,5],[1,2]) should return [1,3,2,4,5]
+// shuffleIt([1,2,3,4,5],[1,2],[3,4]) should return [1,3,2,5,4]
+// shuffleIt([1,2,3,4,5],[1,2],[3,4],[2,3]) should return [1,3,5,2,4]
+
+var shuffleIt=(arr,...ex)=>{
+    for ([a,b] of ex) [arr[a],arr[b]]=[arr[b],arr[a]];
+    return arr;
+  }
