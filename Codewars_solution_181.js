1f5f56cbca5492c8450afd66e4d3fdf4e2df6b73
tonyiboy <antoniocodinggithub@gmail.com>
2023-04-22 12:24:35 +0200

    Codewars solution 181

---
+++ Codewars_solution_181.js
@@ -0,0 +1,5 @@
+// This is an easy twist to the example kata 
+// (provided by Codewars when learning how to create your own kata).
+// Add the value "codewars" to the array websites 1,000 times.
+
+var websites = new Array(1000).fill("codewars");
