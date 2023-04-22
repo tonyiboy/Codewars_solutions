4ac9b3390fd8e7670a8cb4be16fe6a7a1a150e2a
tonyiboy <antoniocodinggithub@gmail.com>
2023-04-22 12:25:14 +0200

    Codewars solution 182

---
+++ Codewars_solution_182.js
@@ -0,0 +1,6 @@
+// This is an easy twist to the example kata 
+// (provided by Codewars when learning how to create your own kata).
+// Add the value "codewars" to the array websites 1,000 times.
+
+var websites = [];
+while (websites.length < 1000) websites.push("codewars")
