492db936efc35eb21263a39436c106a1e0791207
tonyiboy <antoniocodinggithub@gmail.com>
2023-04-24 20:46:02 +0200

    Codewars solution 195

---
+++ Codewars_solution_195.js
@@ -0,0 +1,30 @@
+// DESCRIPTION:
+// You received a whatsup message from an unknown number. 
+// Could it be from that girl/boy with a foreign accent you met yesterday evening?
+// Write a simple function to check if the string contains the word 
+// hallo in different languages.
+// These are the languages of the possible people you met the night before:
+// hello - english
+// ciao - italian
+// salut - french
+// hallo - german
+// hola - spanish
+// ahoj - czech republic
+// czesc - polish
+// Notes
+// you can assume the input is a string.
+// to keep this a beginner exercise you don't need to check if the greeting 
+// is a subset of word (Hallowen can pass the test)
+// function should be case insensitive to pass the tests
+
+function validateHello(greetings) {
+    const message = ["hello", "ciao", "salut", "hallo", "hola", "ahoj", "czesc"];
+     const lowerCaseMessage = greetings.toLowerCase();
+     for (let i = 0; i < message.length; i++) {
+       if (lowerCaseMessage.includes(message[i])) {
+         return true;
+       }
+     }
+     return false;
+   
+   }
