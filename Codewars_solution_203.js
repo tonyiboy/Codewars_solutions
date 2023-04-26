b0b2e0739f4882842ff3165a7fe375905a7b7f75
tonyiboy <antoniocodinggithub@gmail.com>
2023-04-26 18:51:32 +0200

    Codewars solution 203

---
+++ Codewars_solution_203.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Take an array and remove every second element from the array. 
+// Always keep the first element and start removing with the next element.
+// Example:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+// None of the arrays will be empty, so you don't have to worry about that!
+
+const removeEveryOther = arr => arr.filter((_, i) => !(i % 2));
