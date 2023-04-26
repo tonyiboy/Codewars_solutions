42e1cb3d533eacf8dd9ea18347123020ddfb2b45
tonyiboy <antoniocodinggithub@gmail.com>
2023-04-26 18:50:17 +0200

    Codewars solution 201

---
+++ Codewars_solution_201.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Take an array and remove every second element from the array. 
+// Always keep the first element and start removing with the next element.
+// Example:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+// None of the arrays will be empty, so you don't have to worry about that!
+
+function removeEveryOther(arr){
+    return arr.filter(function(elem, index) {
+      return index % 2 === 0;
+    });
+  }
