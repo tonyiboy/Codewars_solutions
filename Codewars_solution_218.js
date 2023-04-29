e934ea5e96eeff3e1e6eea7ae0476516f9911c6c
tonyiboy <antoniocodinggithub@gmail.com>
2023-04-29 15:54:27 +0200

    Codewars solution 218

---
+++ Codewars_solution_218.js
@@ -0,0 +1,5 @@
+// DESCRIPTION:
+// Create a function that takes an integer as an argument and returns 
+// "Even" for even numbers or "Odd" for odd numbers.
+
+let evenOrOdd = n => (n % 2) ? 'Odd' : 'Even';
