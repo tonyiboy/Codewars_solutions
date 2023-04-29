3c3f36e665081f15be806a57dbe85e7241040df6
tonyiboy <antoniocodinggithub@gmail.com>
2023-04-29 15:56:42 +0200

    Codewars solution 220

---
+++ Codewars_solution_220.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Create a function that takes an integer as an argument and returns 
+// "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+    return number & 1 ? 'Odd' : 'Even';
+  }
