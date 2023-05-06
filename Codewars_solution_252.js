ec195d7ae86fd000f19689692b1a12a0ab538568
tonyiboy <antoniocodinggithub@gmail.com>
2023-05-06 12:20:22 +0200

    Codewars solution 252

---
+++ Codewars_solution_252.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Complete the solution so that it reverses all of the words within the string passed in.
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+// Example(Input --> Output):
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+const reverseWords = (str) => str.split(" ").reverse().join(" ")
