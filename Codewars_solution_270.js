4c05318ceea0872054447904a6b7b0e10107ff38
tonyiboy <antoniocodinggithub@gmail.com>
2023-05-09 01:17:14 +0200

    Codewars solution 270

---
+++ Codewars_solution_270.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+// Implement a function which convert the given boolean value into its string representation.
+// Note: Only valid inputs will be given.
+
+function booleanToString(b){
+    if(b === true){
+      return "true";
+    }else{
+      return "false";
+    }
+  }
