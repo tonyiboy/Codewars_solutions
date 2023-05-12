f433e557e11ff2c5ef22b101b165df136e8e879a
tonyiboy <antoniocodinggithub@gmail.com>
2023-05-12 23:25:48 +0200

    Codewars solution 284

---
+++ Codewars_solution_284.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+    // Code here
+    let convert = str.toUpperCase();
+    return convert;
+  }
