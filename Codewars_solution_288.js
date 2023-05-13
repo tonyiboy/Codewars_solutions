ad7a333eeb01de15c7d18bede49d021a58b45388
tonyiboy <antoniocodinggithub@gmail.com>
2023-05-13 23:11:20 +0200

    Codewars solution 288

---
+++ Codewars_solution_288.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Write a function that always returns 5
+// Sounds easy right? Just bear in mind that you 
+// can't use any of the following characters: 0123456789*+-/
+
+function unusualFive() {
+    let five = ["oneFive", "twoFive", "threeFive", "fourFive", "fiveFive"]
+    return five.length;
+    }
