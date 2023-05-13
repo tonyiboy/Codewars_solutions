a01b4a3071f5cc77376d9c87d0f27a3fb1502775
tonyiboy <antoniocodinggithub@gmail.com>
2023-05-13 23:16:15 +0200

    Codewars solution 289

---
+++ Codewars_solution_289.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Write a function that always returns 5
+// Sounds easy right? Just bear in mind that you 
+// can't use any of the following characters: 0123456789*+-/
+
+const unusualFive = () =>
+  "Programming is fun"
+    .indexOf("a");
