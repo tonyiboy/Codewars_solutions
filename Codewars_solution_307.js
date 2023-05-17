f8d78eb4eb3034d06911abb28ac69013200a7f09
tonyiboy <antoniocodinggithub@gmail.com>
2023-05-17 11:24:47 +0200

    Codewars solution 307

---
+++ Codewars_solution_307.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Very simple, given an integer or a floating-point number, find its opposite.
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
+
+const opposite = number => -number
