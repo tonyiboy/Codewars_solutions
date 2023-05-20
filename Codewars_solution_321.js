7187460d7c1ed014e9b1d25499253683fc7281e1
tonyiboy <antoniocodinggithub@gmail.com>
2023-05-20 23:32:00 +0200

    Codewars solution 321

---
+++ Codewars_solution_321.js
@@ -0,0 +1,6 @@
+// DESCRIPTION:
+// Write a function that returns a string in which firstname is swapped with last name.
+// Example(Input --> Output)
+// "john McClane" --> "McClane john"
+
+const nameShuffler = str => str.split(' ').reverse().join(' ');
