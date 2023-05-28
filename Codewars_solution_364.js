9ff5f93a044209e447df4b0a402d14782b130168
tonyiboy <antoniocodinggithub@gmail.com>
2023-05-28 15:08:03 +0200

    Codewars solution 364

---
+++ Codewars_solution_364.js
@@ -0,0 +1,16 @@
+// DESCRIPTION:
+// There are pillars near the road. The distance between the pillars is the same and 
+// the width of the pillars is the same. Your function accepts three arguments:
+// number of pillars (≥ 1);
+// distance between pillars (10 - 30 meters);
+// width of the pillar (10 - 50 centimeters).
+// Calculate the distance between the first and the last pillar in centimeters 
+// (without the width of the first and last pillar).
+
+function pillars(numPill, dist, width) {
+    if(numPill > 1) {
+    return (numPill-2)*width + (numPill-1)*dist*100;
+    } else {
+      return 0;
+    }
+  }
