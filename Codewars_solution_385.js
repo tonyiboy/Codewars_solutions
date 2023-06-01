db868d5e5d9571092c583b64592cc3c2fcba840e
tonyiboy <antoniocodinggithub@gmail.com>
2023-06-01 23:18:30 +0200

    Codewars solution 385

---
+++ Codewars_solution_385.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Build a function that returns an array of integers from n to 1 where n>0.
+// Example : n=5 --> [5,4,3,2,1]
+
+const reverseSeq = n => {
+    return Array.from({length:n},(_,i)=>n-i);
+  };
