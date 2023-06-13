25ce2ef520b585c3522416c0fdd726ad273075d2
tonyiboy <antoniocodinggithub@gmail.com>
2023-06-13 18:41:04 +0200

    Codewars solution 441

---
+++ Codewars_solution_441.js
@@ -0,0 +1,5 @@
+// DESCRIPTION:
+// Make a function that returns the value multiplied by 50 and increased by 6. 
+// If the value entered is a string it should return "Error".
+
+const problem = x => typeof x === 'string' ? 'Error' : x * 50 + 6;
