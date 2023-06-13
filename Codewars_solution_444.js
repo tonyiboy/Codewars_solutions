9793fa2747cd233139bc87adf8d26a82b4a152b5
tonyiboy <antoniocodinggithub@gmail.com>
2023-06-13 18:42:31 +0200

    Codewars solution 444

---
+++ Codewars_solution_444.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// Make a function that returns the value multiplied by 50 and increased by 6. 
+// If the value entered is a string it should return "Error".
+
+function problem(x){
+    let sum = x * 50 + 6;
+    if(x > 0 || x < 0 || x === 0)
+    return sum
+    else {
+    return "Error"
+  }
+  }
+  
