1b296a6887af08bb28e357a1df63c211a58f70e0
tonyiboy <antoniocodinggithub@gmail.com>
2023-06-17 21:56:23 +0200

    Codewars solution 462

---
+++ Codewars_solution_462.js
@@ -0,0 +1,5 @@
+// When provided with a letter, return its position in the alphabet.
+// Input :: "a"
+// Ouput :: "Position of alphabet: 1"
+
+position = first => `Position of alphabet: ${first.charCodeAt() - 96}`;
