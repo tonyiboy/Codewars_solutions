b7d2af9c9996a85283631862651fb893f38f84ac
tonyiboy <antoniocodinggithub@gmail.com>
2023-06-17 21:56:44 +0200

    Codewars solution 463

---
+++ Codewars_solution_463.js
@@ -0,0 +1,7 @@
+// When provided with a letter, return its position in the alphabet.
+// Input :: "a"
+// Ouput :: "Position of alphabet: 1"
+
+function position(letter) {
+    return `Position of alphabet: ${letter.charCodeAt(0) - 96}`
+  }
