7f6b206c734ede04e3b26e37466b17c05f74c146
tonyiboy <antoniocodinggithub@gmail.com>
2023-06-22 22:56:08 +0200

    Codewars solution 487

---
+++ Codewars_solution_487.js
@@ -0,0 +1,6 @@
+// Write a function to split a string and convert it into an array of words.
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", 
+
+const stringToArray = string => string.split(' ');
