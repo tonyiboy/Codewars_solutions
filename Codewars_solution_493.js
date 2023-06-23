365d2686b5764a8b587252b6c63d64dd8f28339e
tonyiboy <antoniocodinggithub@gmail.com>
2023-06-23 22:04:06 +0200

    Codewars solution 493

---
+++ Codewars_solution_493.js
@@ -0,0 +1,16 @@
+// Given a string of digits, you should replace any digit below 5 with '0' 
+// and any digit 5 and above with '1'. Return the resulting string.
+// Note: input will never be an empty string
+
+function fakeBin(str){
+    let newStr = "";
+    for(let i=0;i<str.length;i++){
+      if(Number(str[i])>=5){
+        newStr += "1"
+      }
+      else{
+        newStr += "0";
+      }
+    }
+    return newStr;
+  }
