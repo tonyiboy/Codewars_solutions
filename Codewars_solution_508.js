01bf63ee1fa0695e5f0801b1b4127eeaabcfcbac
tonyiboy <antoniocodinggithub@gmail.com>
2023-06-26 21:44:31 +0200

    Codewars solution 508

---
+++ Codewars_solution_508.js
@@ -0,0 +1,8 @@
+// Write a function get_char() / getChar() which takes a number and 
+// returns the corresponding ASCII char for that value.
+// Example:
+// get_char(65)
+// should return:
+// 'A'
+
+let getChar = c => String.fromCharCode(c)
