3482db57291d95684bf3582305ed00bb558c79c4
tonyiboy <antoniocodinggithub@gmail.com>
2023-06-30 23:20:46 +0200

    Codewars solution 530

---
+++ Codewars_solution_530.js
@@ -0,0 +1,28 @@
+// Given a number n, draw stairs using the letter "I", n tall and n wide, with the tallest in the top left.
+// For example n = 3 result in:
+// "I\n I\n  I"
+// or printed:
+// I
+//  I
+//   I
+// Another example, a 7-step stairs should be drawn like this:
+// I
+//  I
+//   I
+//    I
+//     I
+//      I
+//       I
+
+function drawStairs(n) {
+    // your code here
+    let st  = ""
+    for (let i = 0; i < n ; i++){
+      if (n-i == 1){
+        st += " ".repeat(i) + "I"
+      } else {
+        st += " ".repeat(i) + "I\n"
+        }
+    }
+    return st
+  }
