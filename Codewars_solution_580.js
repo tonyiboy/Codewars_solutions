06eb2443bce41f7e6a6c45eb103f92bd7dafaf74
tonyiboy <antoniocodinggithub@gmail.com>
2023-07-10 22:31:43 +0200

    Codewars solution 580

---
+++ Codewars_solution_580.js
@@ -0,0 +1,4 @@
+// Write a simple function that takes a Date as a parameter and returns a Boolean representing whether the date is today or not.
+// Make sure that your function does not return a false positive by only checking the day.
+
+const isToday = date => date.toDateString() === new Date().toDateString()
