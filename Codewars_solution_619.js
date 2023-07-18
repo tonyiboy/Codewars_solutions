820a60e1cd83d0a867b33253cb19807753ed4a5a
tonyiboy <antoniocodinggithub@gmail.com>
2023-07-18 23:37:46 +0200

    Codewars solution 619

---
+++ Codewars_solution_619.js
@@ -0,0 +1,14 @@
+// // Task
+// // Give you two strings: s1 and s2. If they are opposite, return true; otherwise, return false. 
+// // Note: The result should be a boolean value, instead of a string.
+// // The opposite means: All letters of the two strings are the same, but the case is opposite. 
+// // you can assume that the string only contains letters or it's a empty string. Also take note of 
+// the edge case - if both strings are empty then you should return false/False.
+// // Examples (input -> output)
+// // "ab","AB"     -> true
+// // "aB","Ab"     -> true
+// // "aBcd","AbCD" -> true
+// // "AB","Ab"     -> false
+// // "",""         -> false
+
+const isOpposite=(s1,s2)=>!s1=="" && s1.split('').map(b=>b.toLowerCase() == b ? b.toUpperCase() : b.toLowerCase()).join('')==s2;
