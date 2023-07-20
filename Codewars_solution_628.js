8867cb952795412b0089ff4ce0a89ac8e98294a0
tonyiboy <antoniocodinggithub@gmail.com>
2023-07-20 22:02:35 +0200

    Codewars solution 628

---
+++ Codewars_solution_628.js
@@ -0,0 +1,11 @@
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(n){
+    
+    if(n%2===0 ){
+     return n *8
+    }
+    else{
+    return n* 9;
+    }    
+}
