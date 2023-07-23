8a88574e255ae9c956cb74b35d8389ca6c88d183
tonyiboy <antoniocodinggithub@gmail.com>
2023-07-23 18:23:10 +0200

    Codewars solution 642

---
+++ Codewars_solution_642.js
@@ -0,0 +1,6 @@
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+// The order of the sequence has to stay the same.
+
+function distinct(a) {
+    return Array.from(new Set(a));
+  }
