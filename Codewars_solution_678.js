5e219f1f3f16cea94a18b537eed64f25ccae93ea
tonyiboy <antoniocodinggithub@gmail.com>
2023-07-30 22:48:54 +0200

    Codewars solution 678

---
+++ Codewars_solution_678.js
@@ -0,0 +1,10 @@
+// Your task is simply to count the total number of lowercase letters in a string.
+// Examples
+// "abc" ===> 3
+// "abcABC123" ===> 3
+// "abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 3
+// "" ===> 0;
+// "ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 0
+// "abcdefghijklmnopqrstuvwxyz" ===> 26
+
+const lowercaseCount = str => str.replace(/[^a-z]/g, '').length
