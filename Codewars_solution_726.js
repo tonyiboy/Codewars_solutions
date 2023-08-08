a551cfea8e7626ecbbc19aad79c61e84d33df064
tonyiboy <antoniocodinggithub@gmail.com>
2023-08-09 00:19:18 +0200

    Codewars solution 726

---
+++ Codewars_solution_726.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// Summation
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+// For example (Input -> Output):
+// 2 -> 3 (1 + 2)
+// 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+var summation = function (num) {
+    let result = 0;
+    for (var i = 1; i <= num; i++) {
+      result += i;
+    }
+    
+    return result;
+  }
