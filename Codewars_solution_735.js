70ae777e0fa7475c94172099230ac4b13aacc936
tonyiboy <antoniocodinggithub@gmail.com>
2023-08-11 23:04:03 +0200

    Codewars solution 735

---
+++ Codewars_solution_735.js
@@ -0,0 +1,7 @@
+// Wilson primes satisfy the following condition. Let P represent a prime number.
+// Then,
+// ((P-1)! + 1) / (P * P)
+// should give a whole number.
+// Your task is to create a function that returns true if the given number is a Wilson prime.
+
+const amIWilson = p => p==5 || p==13 || p==563
