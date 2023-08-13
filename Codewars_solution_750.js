d9e8a547118256e2e9d2dbdba0e4c60a364e77ef
tonyiboy <antoniocodinggithub@gmail.com>
2023-08-13 23:13:41 +0200

    Codewars solution 750

---
+++ Codewars_solution_750.js
@@ -0,0 +1,13 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+    let sum = 0
+    
+    for (let num of arr) {
+      if (num > 0) sum += num
+    }
+  
+    return sum
+  }
