683138a12ad6a1487cad28a909c760b04cdb746f
tonyiboy <antoniocodinggithub@gmail.com>
2023-08-17 21:48:26 +0200

    Codewars solution 767

---
+++ Codewars_solution_767.js
@@ -0,0 +1,10 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+const grow = x => {
+    let res = 1;
+    for (let i = 0; i < x.length; i++) {
+      res *= x[i];
+    }
+    return res;
+  }
