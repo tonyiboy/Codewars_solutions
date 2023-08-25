56cf9c51dfc393de07312f48d1a1a64c13f5da11
tonyiboy <antoniocodinggithub@gmail.com>
2023-08-25 21:27:09 +0200

    Codewars solution 807

---
+++ Codewars_solution_807.js
@@ -0,0 +1,5 @@
+// You are given two interior angles (in degrees) of a triangle.
+// Write a function to return the 3rd.
+// Note: only positive integers will be tested.
+
+const otherAngle = (a, b) => 180 - a - b
