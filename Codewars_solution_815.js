bea0e3ea82ced688c4d9d1feb73225e92085ae9a
tonyiboy <antoniocodinggithub@gmail.com>
2023-08-26 23:25:50 +0200

    Codewars solution 815

---
+++ Codewars_solution_815.js
@@ -0,0 +1,6 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+// For example, for [1, 2, 2] it should return 9 
+
+function squareSum(numbers) {
+    return numbers.map(n => n ** 2).reduce((a, b) => a + b, 0);
+  }
