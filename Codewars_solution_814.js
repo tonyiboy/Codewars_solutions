20e062a7eac8966143249380495a62cec57c0d0e
tonyiboy <antoniocodinggithub@gmail.com>
2023-08-26 23:25:07 +0200

    Codewars solution 814

---
+++ Codewars_solution_814.js
@@ -0,0 +1,11 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+// For example, for [1, 2, 2] it should return 9 
+
+
+function squareSum(numbers){
+    let result = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      result += (numbers[i] * numbers[i]);
+    }
+    return result;
+  }
