52e6d38290c17e34af638d6b0773efecbc0fec0b
tonyiboy <antoniocodinggithub@gmail.com>
2023-08-28 17:47:59 +0200

    Codewars solution 821

---
+++ Codewars_solution_821.js
@@ -0,0 +1,6 @@
+// You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all printable ASCII characters.
+// Examples:
+// uniTotal("a") == 97
+// uniTotal("aaa") == 291
+
+const uniTotal = str => [...str].reduce((acc, char) => acc + char.charCodeAt(0), 0);
