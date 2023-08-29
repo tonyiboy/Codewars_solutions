b5c7d97da1360ce65358aa1c875f42bfddc37f61
tonyiboy <antoniocodinggithub@gmail.com>
2023-08-29 17:57:30 +0200

    Codewars solution 827

---
+++ Codewars_solution_827.js
@@ -0,0 +1,8 @@
+// This series of katas will introduce you to basics of doing geometry with computers.
+// Point objects have attributes x and y.
+// Write a function calculating distance between Point a and Point b.
+// Tests compare expected result and actual answer with tolerance of 1e-6.
+
+function distanceBetweenPoints(a, b) {
+    return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+  }
