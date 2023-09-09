26e59af91cb6df258f59ca5f440f26e124867174
tonyiboy <antoniocodinggithub@gmail.com>
2023-09-09 21:22:05 +0200

    Codewars solution 881

---
+++ Codewars_solution_881.js
@@ -0,0 +1,6 @@
+// When provided with a number between 0-9, return it in words.
+// Input :: 1
+// Output :: "One".
+// If your language supports it, try using a switch statement.
+
+switchItUp=n=>["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n]
