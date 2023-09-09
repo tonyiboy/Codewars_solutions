bbd1314afe4c2c6b72afa7162668e413d3d2f07d
tonyiboy <antoniocodinggithub@gmail.com>
2023-09-09 21:26:06 +0200

    Codewars solution 885

---
+++ Codewars_solution_885.js
@@ -0,0 +1,9 @@
+// When provided with a number between 0-9, return it in words.
+// Input :: 1
+// Output :: "One".
+// If your language supports it, try using a switch statement.
+
+function switchItUp(number){
+    let words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+    return words[number];
+  }
