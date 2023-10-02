36d7a10b2a379ccf685d69b30ab2ac55e0675e05
tonyiboy <antoniocodinggithub@gmail.com>
2023-10-02 17:36:04 +0200

    Codewars solution 999

---
+++ Codewars_solution_999.js
@@ -0,0 +1,14 @@
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+// Examples(Input1, Input2 --> Output):
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+const rps = (p1, p2) => {
+    if (p1 === p2) return 'Draw!';
+    if (p1 === 'rock' && p2 === 'scissors') return 'Player 1 won!';
+    if (p1 === 'scissors' && p2 === 'paper') return 'Player 1 won!';
+    if (p1 === 'paper' && p2 === 'rock') return 'Player 1 won!';
+    return 'Player 2 won!';
+  };
