86d7d8fc7795ccd6123015b5dfbd124f173e43d3
tonyiboy <antoniocodinggithub@gmail.com>
2023-10-04 20:01:47 +0200

    Codewars solution 1010

---
+++ Codewars_solution_1010.js
@@ -0,0 +1,5 @@
+// Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+
+// Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+
+const squareArea = A => Math.round(Math.pow((2*A/Math.PI),2)*100)/100
