0f2cfc43d67e4559c6d497181ace7285dbd38df9
tonyiboy <antoniocodinggithub@gmail.com>
2023-10-07 13:55:49 +0200

    Codewars solution 1025

---
+++ Codewars_solution_1025.js
@@ -0,0 +1,10 @@
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+function getASCII(c){
+    let index = 0;
+    
+    let result = c.charCodeAt(index);
+      return result
+    }
