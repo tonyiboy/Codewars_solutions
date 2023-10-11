79835c3dfffb25d6103f8c47db3fcc4cd801f988
tonyiboy <antoniocodinggithub@gmail.com>
2023-10-11 14:23:13 +0200

    Codewars solution 1042

---
+++ Codewars_solution_1042.js
@@ -0,0 +1,15 @@
+// Color Ghost
+// Create a class Ghost
+
+// Ghost objects are instantiated without any arguments.
+
+// Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+// ghost = new Ghost();
+// ghost.color //=> "white" or "yellow" or "purple" or "red"
+
+var Ghost = function() {
+    let colors = ["white", "yellow", "purple", "red"];
+    let colorIndex = Math.floor(Math.random() * (colors.length));
+    this.color = colors[colorIndex];
+  };
