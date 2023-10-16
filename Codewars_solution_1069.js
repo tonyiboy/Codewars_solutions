248c2c9e8f0530a5c5c983ea497b928a32a91db1
tonyiboy <antoniocodinggithub@gmail.com>
2023-10-16 04:28:25 +0200

    Codewars solution 1069

---
+++ Codewars_solution_1069.js
@@ -0,0 +1,18 @@
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// Our deck (is preloaded):
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+function defineSuit(card) {
+    switch (card.slice(-1)){
+      case '♣': return 'clubs';
+      case '♦': return 'diamonds';
+      case '♥': return 'hearts';
+      case '♠': return 'spades';    
+    }
+      
+  }
