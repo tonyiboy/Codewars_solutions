4f8daa041432726d986f835e250b65e4081d726f
tonyiboy <antoniocodinggithub@gmail.com>
2023-10-22 00:44:27 +0200

    Codewars solution 1099

---
+++ Codewars_solution_1099.js
@@ -0,0 +1,14 @@
+// Description:
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi!!"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
+
+function remove(s){
+    return s[s.length - 1] == '!' ? s.slice(0, -1) : s;
+  }
