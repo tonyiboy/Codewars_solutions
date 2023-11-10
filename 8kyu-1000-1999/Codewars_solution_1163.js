b527db2b0ff87e2913489cbe687bd6a2a2b1ce0d
tonyiboy <antoniocodinggithub@gmail.com>
2023-11-10 23:22:26 +0100

    Codewars_solution_1163

---
+++ 8kyu-1000-1999/Codewars_solution_1163.js
@@ -0,0 +1,7 @@
+// Total pressure calculation
+
+solution= (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
+    let result = (((givenMass1 / molarMass1) + (givenMass2 / molarMass2)) * 0.082 * (temp + 273.15)) / volume;
+    return result;
+    
+  }
