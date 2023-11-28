f814f2e99bc32ce55c869b3f9f6b083ef4c51859
tonyiboy <antoniocodinggithub@gmail.com>
2023-11-28 23:46:21 +0100

    Codewars_solution_1195.js

---
+++ 8kyu-1000-1999/Codewars_solution_1195.js
@@ -0,0 +1,47 @@
+// Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+// Please use the following function names:
+
+// addition = add
+
+// multiply = multiply
+
+// division = divide (both integer and float divisions are accepted)
+
+// modulus = mod
+
+// exponential = exponent
+
+// subtraction = subt
+
+// Note: All math operations will be: a (operation) b
+
+function add(a,b){
+    let result = a+b
+    return result
+ }
+ 
+ function divide(a,b){
+    let result = a/b 
+   return result
+ }
+ 
+ function multiply(a,b){
+   let result = a*b 
+   return result
+ }
+ 
+ function mod(a,b){
+   let result = a%b  
+   return result
+ }
+    
+ function exponent(a,b){
+   let result = a**b  
+   return result
+ }
+     
+ function subt(a,b){
+  let result = a-b
+   return result
+ }
