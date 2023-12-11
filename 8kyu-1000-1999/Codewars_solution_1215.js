7220f01547602839a21e61b15b974d735e5856d8
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-11 10:33:32 +0100

    Codewars_solution_1215.js

---
+++ 8kyu-1000-1999/Codewars_solution_1215.js
@@ -0,0 +1,5 @@
+// Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+
+function logs(x , a, b){
+    return Math.log(a) / Math.log(x) + Math.log(b) / Math.log(x);
+  }
