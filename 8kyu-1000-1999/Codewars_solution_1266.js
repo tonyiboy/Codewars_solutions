1d176009a2a905d61bd7c2db3c79e0ad82062827
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-20 19:22:34 +0100

    Codewars_solution_1266.js

---
+++ 8kyu-1000-1999/Codewars_solution_1266.js
@@ -0,0 +1,15 @@
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. 
+// All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+function getAverage(marks){
+    let sum = 0;
+    
+    for (var i in marks)
+      sum += marks[i];
+    
+    return parseInt(sum / marks.length);
+  }
