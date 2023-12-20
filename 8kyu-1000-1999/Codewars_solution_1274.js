68fb52590e66d6aaf3ba264f4c354ead9f87d6d8
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-20 19:57:54 +0100

    Codewars_solution_1274.js

---
+++ 8kyu-1000-1999/Codewars_solution_1274.js
@@ -0,0 +1,15 @@
+// Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. 
+// First argument is an array of numbers and the second is the divisor.
+
+// Example(Input1, Input2 --> Output)
+// [1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+
+function divisibleBy(numbers, divisor) {
+	var newArr = [];
+	for (let i = 0; i < numbers.length; i++) {
+		if (numbers[i] % divisor === 0) {
+			newArr.push(numbers[i]);
+		} 
+	}
+return newArr;
+}
