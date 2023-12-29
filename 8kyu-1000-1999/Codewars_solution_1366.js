5986c624240eb2bb51db9123bd11880a53aa0932
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-29 03:43:52 +0100

    Codewars_solution_1366.js

---
+++ 8kyu-1000-1999/Codewars_solution_1366.js
@@ -0,0 +1,26 @@
+// Consider an array/list of sheep where some sheep may be missing from their place. 
+// We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+function countSheeps(sheep) {
+    let arr = 0
+    let counter = 0
+    while(counter < sheep.length){
+      if (sheep[counter]){
+        arr += 1
+      }
+       counter++
+    }
+    return arr
+  }
