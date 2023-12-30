696a1d660129916aabeb4a90e0432a5b81be7f5f
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-30 17:08:02 +0100

    Codewars_solution_1373.js

---
+++ 8kyu-1000-1999/Codewars_solution_1373.js
@@ -0,0 +1,29 @@
+// Task
+// Create a function that always returns true for every item in a given list. However, if an element is the word "flick", 
+// switch to always returning the opposite boolean value.
+
+// Examples
+// ['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
+
+// ['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
+
+// ['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
+// Notes
+// "flick" will always be given in lowercase.
+// A list may contain multiple flicks.
+// Switch the boolean value on the same element as the flick itself.
+
+function flickSwitch(arr){
+    let noArr = []
+    let mySwitch = true
+    let count = 0
+    
+    while(count <= arr.length -1){
+      if (arr[count] === "flick") {
+        mySwitch = !mySwitch
+        noArr.push(mySwitch)
+      }else noArr.push(mySwitch)
+      count++
+    }
+    return noArr
+  }
