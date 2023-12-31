cc29d56ef180f14f126206ace14e9e0ff776981d
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-31 23:43:16 +0100

    Codewars_solution_1386.js

---
+++ 8kyu-1000-1999/Codewars_solution_1386.js
@@ -0,0 +1,7 @@
+// Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+// Function:
+
+// getNumberFromString(s)
+
+let getNumberFromString = s => +s.replace(/\D/g, "")
