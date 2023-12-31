0c5dff272bcb0e0083ff3b6708595fc18daacba1
tonyiboy <antoniocodinggithub@gmail.com>
2023-12-31 23:45:53 +0100

    Codewars_solution_1389.js

---
+++ 8kyu-1000-1999/Codewars_solution_1389.js
@@ -0,0 +1,7 @@
+// Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+// Function:
+
+// getNumberFromString(s)
+
+const getNumberFromString = s => parseInt(s.match(/\d/g).join(''))
