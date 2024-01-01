4f5c7874835b44ee388a601b5534136623f1fad3
tonyiboy <antoniocodinggithub@gmail.com>
2024-01-01 22:46:21 +0100

    Codewars_solution_1400.js

---
+++ 8kyu-1000-1999/Codewars_solution_1400.js
@@ -0,0 +1,21 @@
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. 
+// If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. 
+// If there are no good ideas, as is often the case, return 'Fail!'.
+
+function well(x){
+    
+    let count = 0
+    let counting = 0
+    
+    while(counting< x.length){
+      if(x[counting] === "good"){
+        count += 1
+      }
+      counting++
+    }
+    
+    return count ? count > 2 ? 'I smell a series!' : 'Publish!' : 'Fail!'
+    
+  }
