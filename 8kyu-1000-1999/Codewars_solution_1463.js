2505c9dbb55254063b59672a5f4f274d48fbb211
tonyiboy <antoniocodinggithub@gmail.com>
2024-01-07 10:41:51 +0100

    Codewars_solution_1463.js

---
+++ 8kyu-1000-1999/Codewars_solution_1463.js
@@ -0,0 +1,37 @@
+// Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. 
+// Fortunately, you are good at spotting them.
+
+// Warn the sheep in front of the wolf that it is about to be eaten. 
+// Remember that you are standing at the front of the queue which is at the end of the array:
+
+// [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+//    7      6      5      4      3            2      1
+// If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep".
+//  Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+
+// Note: there will always be exactly one wolf in the array.
+
+// Examples
+// Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
+// Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+
+// Input: ["sheep", "sheep", "wolf"]
+// Output: "Pls go away and stop eating my sheep"
+
+function warnTheSheep(queue) {
+    let result
+    let resultSheep = 0
+    let arrNumber = queue.length 
+   
+    for(let i = 0; i < arrNumber; i++){
+      if(queue[i] === "wolf"){
+        result = i 
+        for(let i = result; i < arrNumber -1; i++ ){
+        resultSheep += 1
+        }
+      }
+    }
+   
+    let word = result === arrNumber -1 ? "Pls go away and stop eating my sheep" : `Oi! Sheep number ${resultSheep}! You are about to be eaten by a wolf!`
+    return word
+  }
