c77cd7923e97300c9ac1e5d6a1eb53d7d9b23344
tonyiboy <antoniocodinggithub@gmail.com>
2024-01-08 06:59:20 +0100

    Codewars_solution_1474.js

---
+++ 8kyu-1000-1999/Codewars_solution_1474.js
@@ -0,0 +1,5 @@
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+function include(arr, item){
+    return arr.some(element => element === item)
+  }  
