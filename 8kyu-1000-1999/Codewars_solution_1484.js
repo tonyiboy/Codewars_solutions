d46412f6fcf143302852f1c15630ce260042fc60
tonyiboy <antoniocodinggithub@gmail.com>
2024-01-09 23:31:07 +0100

    Codewars_solution_1484.js

---
+++ 8kyu-1000-1999/Codewars_solution_1484.js
@@ -0,0 +1,19 @@
+// The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+// But it's not working properly.
+
+// Task
+// Fix the bug so we can all go home early.
+
+// Notes
+// String str will never be null.
+
+var replaceDots = function (str) {
+  
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === ".") {
+        str = str.split('.').join('-') 
+      }
+    }
+    return str;
+  }
