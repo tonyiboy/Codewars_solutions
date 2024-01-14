9a61aaa8ad6747b754a37f758d356a560032ef40
tonyiboy <antoniocodinggithub@gmail.com>
2024-01-14 11:41:21 +0100

    Codewars_solution_1537.js

---
+++ 8kyu-1000-1999/Codewars_solution_1537.js
@@ -0,0 +1,6 @@
+// Time to test your basic knowledge in functions! Return the odds from a list:
+
+// [1, 2, 3, 4, 5]  -->  [1, 3, 5]
+// [2, 4, 6]        -->  []
+
+let odds = values => values.filter( el => el % 2)
