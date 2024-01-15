9b249bb2fffe412249406f464d37864c14de072d
tonyiboy <antoniocodinggithub@gmail.com>
2024-01-15 11:22:14 +0100

    Codewars_solution_1545.js

---
+++ 8kyu-1000-1999/Codewars_solution_1545.js
@@ -0,0 +1,24 @@
+// Unfinished Loop - Bug Fixing #1
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+// function createArray(number){
+//     var newArray = [];
+    
+//     for(var counter = 1; counter <= number;){
+//       newArray.push(counter);
+//     }
+    
+//     return newArray;
+//   }
+
+function createArray(number){
+    var newArray = [];
+    
+    let counter = 1
+    do{
+      newArray.push(counter);
+      counter++
+    }while(counter <= number)
+    
+    return newArray;
+  }
