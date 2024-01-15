ce0080d28ad566bdacb3cacff33b8506584125e2
tonyiboy <antoniocodinggithub@gmail.com>
2024-01-15 11:10:40 +0100

    Codewars_solution_1540.js

---
+++ 8kyu-1000-1999/Codewars_solution_1540.js
@@ -0,0 +1,23 @@
+// Functional closures can get overly attached. Set them straight!
+
+// Why doesn't greetAbe() actually greet Abe?
+
+// let name = 'Abe'
+
+// const greetAbe = () => 'Hello, ' + name + '!'
+
+// name = 'Ben'
+
+// const greetBen = () => 'Hello, ' + name + '!'
+
+let name
+function greetAbe() {
+  name = 'Abe'
+  return "Hello, " + name + '!'
+}
+
+
+function greetBen() {
+  name = 'Ben' 
+  return "Hello, " + name + '!'
+}
