8126985e5b32dead59d60015d60e9a686a8388d2
tonyiboy <antoniocodinggithub@gmail.com>
2024-01-16 12:37:10 +0100

    Codewars_solution_1555.js

---
+++ 8kyu-1000-1999/Codewars_solution_1555.js
@@ -0,0 +1,13 @@
+// Combine strings function
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+// returns:
+
+// 'James Stevens'
+
+function combineNames(...names){
+    return names.join(" ")
+}
