713cdeb495816b598290c968d34525ec7db4ff34
tonyiboy <antoniocodinggithub@gmail.com>
2024-01-19 14:08:47 +0100

    Codewars_solution_1583.js

---
+++ 8kyu-1000-1999/Codewars_solution_1583.js
@@ -0,0 +1,22 @@
+// Shouldn't the two functions getMax1 and getMax2 be equivalent and return the same value? 
+// Can you spot a problem and fix it? Can you learn something about JavaScript style in this kata?
+
+// function getMax1()
+// {
+//   var max = 
+//   {
+//    name: 'Max Headroom'
+//   }
+//   return max;
+// }
+
+// function getMax2()
+// {
+//   return
+//   {
+//     name: 'Max Headroom'
+//   }
+// }
+
+const getMax1 = () => ({ name: 'Max Headroom' })
+const getMax2 = () => ({ name: 'Max Headroom' })
