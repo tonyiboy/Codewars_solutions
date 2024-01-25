434691b52a19ba8aebe0d636c4030d5acba9a970
tonyiboy <antoniocodinggithub@gmail.com>
2024-01-25 18:19:06 +0100

    Codewars_solution_1604.js

---
+++ 8kyu-1000-1999/Codewars_solution_1604.js
@@ -0,0 +1,22 @@
+// Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
+
+// You need to consider the following ratings:
+
+// Terrible: tip 0%
+// Poor: tip 5%
+// Good: tip 10%
+// Great: tip 15%
+// Excellent: tip 20%
+// The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating is received, then you need to return:
+
+// "Rating not recognised" in Javascript, Python and Ruby...
+// ...or null in Java
+// ...or -1 in C#
+// Because you're a nice person, you always round up the tip, regardless of the service.
+
+function calculateTip(amount, rating) {
+    let rate = rating.toLowerCase();
+    
+    return rate === "excellent" ? Math.ceil(amount*20/100) : rate === "great" ? Math.ceil(amount*15/100) : rate === "good" ? Math.ceil(amount*10/100) : rate === "poor" ? Math.ceil(amount*5/100) : rate === "terrible" ? Math.ceil(amount*0/100) : "Rating not recognised"
+    
+    }
