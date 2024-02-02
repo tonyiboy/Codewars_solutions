fca126b209bbef589ec1c703d607022884a23329
tonyiboy <antoniocodinggithub@gmail.com>
2024-02-02 03:56:09 +0100

    Codewars_7kyu_solution_24.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_24.js
@@ -0,0 +1,21 @@
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+// Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+
+// Note: The function accepts an integer and returns an integer.
+
+// Happy Coding!
+
+function squareDigits(number){
+    let square = []
+    let arr = number.toString().split("")
+    let count = 0
+    
+    do{
+      square += arr[count] * arr[count]
+      count++
+    }while(count < arr.length)
+    return +square
+  }
