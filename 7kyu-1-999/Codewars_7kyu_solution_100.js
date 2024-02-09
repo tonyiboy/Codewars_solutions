0fcb9444f2a35bf78b2d241f116bd73eaf591e78
tonyiboy <antoniocodinggithub@gmail.com>
2024-02-09 13:19:12 +0100

    Codewars_7kyu_solution_100.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_100.js
@@ -0,0 +1,17 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(str) {
+    let arr = []
+    let count = 0
+    while (count < str.length) {
+      arr.push(str.charAt(count).toUpperCase() + str.charAt(count).toLowerCase().repeat(count))
+      count++
+    }
+    return arr.join("-")
+  }
