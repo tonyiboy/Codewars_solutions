4572fd81b1421fa467ba754f92c33e79f9f305c7
tonyiboy <antoniocodinggithub@gmail.com>
2024-02-10 14:56:35 +0100

    Codewars_7kyu_solution_115.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_115.js
@@ -0,0 +1,26 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+// Your function should only return a number, not the explanation about how you get that number.
+
+function getSum( a,b )
+{
+  let min = Math.min(a,b);
+  let max = Math.max(a,b)
+  let result = 0;
+  
+  while(min <=max ){
+    result += min
+    min++
+  }
+
+  return result
+}
