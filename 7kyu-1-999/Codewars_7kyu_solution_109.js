ac51766abc7d54fcde60495afe2dc300a59d44d9
tonyiboy <antoniocodinggithub@gmail.com>
2024-02-10 14:40:14 +0100

    Codewars_7kyu_solution_109.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_109.js
@@ -0,0 +1,8 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+let solution = (str, ending) => str.substr(str.length - ending.length, str.length) === ending 
