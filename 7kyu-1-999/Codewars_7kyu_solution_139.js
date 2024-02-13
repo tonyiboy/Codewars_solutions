b9e5aed9b54c3076e34631e5f9216d9eb063c98d
tonyiboy <antoniocodinggithub@gmail.com>
2024-02-13 01:00:40 +0100

    Codewars_7kyu_solution_139.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_139.js
@@ -0,0 +1,12 @@
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, 
+// the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+let longest = (s1, s2) => Array.from(new Set(s1 + s2)).sort().join('')
