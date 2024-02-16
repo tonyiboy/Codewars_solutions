bead6adcca594def2e97c5359fbb76ff5f14f15f
tonyiboy <antoniocodinggithub@gmail.com>
2024-02-16 01:06:57 +0100

    Codewars_7kyu_solution_172.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_172.js
@@ -0,0 +1,23 @@
+// TM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Examples (Input --> Output)
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+function validatePIN (pin) {
+ 
+	let result = pin.length
+   if( result != 4 && result != 6){ 
+  		return false
+  }
+  let count = 0
+  do{
+    if (pin[count] > '9' || pin[count] < '0'){
+      return false
+    }count++
+    }while(count < pin.length)
+  return true
+}
