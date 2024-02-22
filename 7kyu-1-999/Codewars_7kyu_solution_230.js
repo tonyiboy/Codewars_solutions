5e4d590a5faf1cbfa18bc61f1d89918dcaec125c
tonyiboy <antoniocodinggithub@gmail.com>
2024-02-22 04:00:11 +0100

    Codewars_7kyu_solution_230.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_230.js
@@ -0,0 +1,17 @@
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+// Examples
+// [1, 1, 2] ==> 2
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+
+function stray(numbers) {
+    for(let i = 0; i < numbers.length; i++){
+      if(numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])) {
+        return numbers[i]
+      }
+    }
+  }
