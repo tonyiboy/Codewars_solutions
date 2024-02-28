37e2c4cf080214ebc51eaa53a6a93e6bb6843610
tonyiboy <antoniocodinggithub@gmail.com>
2024-02-28 08:07:16 +0100

    Codewars_7kyu_solution_299.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_299.js
@@ -0,0 +1,28 @@
+// Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. 
+// If the number is prime return the string '(integer) is prime' (null in C#, empty table in COBOL) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+// Example:
+// divisors(12); // should return [2,3,4,6]
+// divisors(25); // should return [5]
+// divisors(13); // should return "13 is prime"
+
+function divisors(integer) {
+    let arr = []
+    let count = 2
+    
+    do{
+      if(integer % count == 0) {
+       arr.push(count)
+      } 
+      count++
+    }while(count < integer)
+      
+      switch(true){
+          case arr.length !== 0:
+          return arr;
+          break;
+          default:
+          return `${integer} is prime`;
+          break;
+      }
+  }
