d7a1a8629b46e898a5d0f657a28d1d5fac01f1a4
tonyiboy <antoniocodinggithub@gmail.com>
2024-02-29 04:53:11 +0100

    Codewars_7kyu_solution_309.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_309.js
@@ -0,0 +1,15 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url){
+    let string = ""
+    
+    for(let i = 0; i < url.length; i++){
+      if(url[i] === "#") { break }
+      string += url[i]
+    }
+    return string 
+  }
