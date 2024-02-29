830b204d6f11a8c1e45bf861460fcb7da55e24bd
tonyiboy <antoniocodinggithub@gmail.com>
2024-02-29 04:40:13 +0100

    Codewars_7kyu_solution_303.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_303.js
@@ -0,0 +1,7 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+let removeUrlAnchor = url => url.split('#').shift()
