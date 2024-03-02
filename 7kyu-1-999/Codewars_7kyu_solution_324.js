fc7cf0a13cbf4b2d98ffc06e049a18de535bde27
tonyiboy <antoniocodinggithub@gmail.com>
2024-03-02 10:21:14 +0100

    Codewars_7kyu_solution_324.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_324.js
@@ -0,0 +1,13 @@
+// Instructions
+// Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, 
+// and returns an ordered list containing the indices of all capital (uppercase) letters in the string.
+
+// Example (Input --> Output)
+// "CodEWaRs" --> [0,3,4,6]
+
+function capitals(word) {
+	
+	return word.split('').reduce(function(a, b, c) {
+    return b === b.toUpperCase() ? a.concat(c) : a;
+  }, []);
+}
