df82687e68fa3a9fb64ed34cc11f588eb96981f2
tonyiboy <antoniocodinggithub@gmail.com>
2024-03-16 22:35:59 +0100

    Codewars_7kyu_solution_435.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_435.js
@@ -0,0 +1,11 @@
+// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+// The solution would work like the following:
+
+// getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+function getEvenNumbers(numbersArray){
+    
+    let result = numbersArray.filter(n => !(n % 2))
+    return result
+    }
