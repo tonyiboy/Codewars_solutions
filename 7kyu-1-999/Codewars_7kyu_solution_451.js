34a258217efc23629d7ad91ee5c6a7241866f111
tonyiboy <antoniocodinggithub@gmail.com>
2024-03-18 16:12:54 +0100

    Codewars_7kyu_solution_451.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_451.js
@@ -0,0 +1,16 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+function noOdds( values ){
+  
+    let result = []
+    
+    for(let i in values){
+        if(values[i] % 2 === 0){
+          result.push(values[i])
+        }
+    }
+    
+    return result
+  }
