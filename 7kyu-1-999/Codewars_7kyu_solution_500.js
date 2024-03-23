dc0fb9b523ccc162f50e0ff0fe4c4dc5227c2586
tonyiboy <antoniocodinggithub@gmail.com>
2024-03-23 02:09:09 +0100

    Codewars_7kyu_solution_500.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_500.js
@@ -0,0 +1,20 @@
+// vYour car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+// Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). 
+// If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+
+function bump(x) {
+    let result = 0
+    let count = 0
+    
+    while(count < x.length) {
+      if (x[count] === 'n'){
+        result ++ 
+      }  
+      count++
+    }
+   
+    if(result > 15){
+      return "Car Dead"
+    }else return "Woohoo!"
+  }
