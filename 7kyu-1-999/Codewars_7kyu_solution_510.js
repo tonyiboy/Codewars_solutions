c9d5227511d700599f69b496fe45799ed92cb052
tonyiboy <antoniocodinggithub@gmail.com>
2024-03-24 22:35:12 +0100

    Codewars_7kyu_solution_510.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_510.js
@@ -0,0 +1,16 @@
+// Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
+
+// Return as a number.
+
+function divCon(x){
+    let result = 0
+     
+    for(let i = 0; i < x.length; i++){
+       if(typeof x[i] === 'number'){
+          result += x[i]
+        }else{
+          result -= x[i]
+        }    
+    }
+    return result
+  }
