37d0612bdbb472fe782713360dcc84592ac7457e
tonyiboy <antoniocodinggithub@gmail.com>
2024-03-26 22:40:47 +0100

    Codewars_7kyu_solution_530.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_530.js
@@ -0,0 +1,6 @@
+// Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+function angle(n) {
+    let result = (n - 2) * 180
+    return result
+  }
