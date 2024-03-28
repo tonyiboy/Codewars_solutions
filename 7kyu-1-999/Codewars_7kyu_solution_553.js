462fa15498a86743662292d0e85d0697c43aaf01
tonyiboy <antoniocodinggithub@gmail.com>
2024-03-28 01:19:38 +0100

    Codewars_7kyu_solution_553.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_553.js
@@ -0,0 +1,26 @@
+// Task
+// Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+// Notes
+// Array/list size is at least 2.
+
+// Array/list numbers could be a mixture of positives, negatives also zeroes .
+
+// Input >> Output Examples
+// adjacentElementsProduct([1, 2, 3]); ==> return 6
+// Explanation:
+// The maximum product obtained from multiplying 2 * 3 = 6, and they're adjacent numbers in the array.
+// adjacentElementsProduct([9, 5, 10, 2, 24, -1, -48]); ==> return 50
+// Explanation:
+// Max product obtained from multiplying 5 * 10  =  50 .
+
+function adjacentElementsProduct(array) {
+    let result =[]
+    let count = 0
+    
+    do{
+      result.push(array[count] * array[count + 1])
+      count++
+    }while(count < array.length - 1)
+    return Math.max(...result)
+  }
