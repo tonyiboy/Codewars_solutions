2c34c63ea2f4acb267aaf4291f118d2a29539fcb
tonyiboy <antoniocodinggithub@gmail.com>
2024-03-30 20:29:20 +0100

    Codewars_7kyu_solution_567.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_567.js
@@ -0,0 +1,17 @@
+// Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+
+// @ @@ @ @@ @ @@ @ @@ @ @@ @
+
+// Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
+// If there are less than 2 blue beads return 0.
+
+function countRedBeads(n) {
+    switch(true){
+        case n < 2:
+        return 0;
+        break;
+        default: 
+        return (n - 1) * 2;
+        break;
+   }
+ }
