3ca5318122291a1332654f4aa624fe86dbfe0535
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-01 16:10:58 +0200

    Codewars_7kyu_solution_581.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_581.js
@@ -0,0 +1,11 @@
+// Given a sequence of numbers, find the largest pair sum in the sequence.
+
+// For example
+
+// [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+// [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+// Input sequence contains minimum two elements and every element is an integer.
+
+function largestPairSum(numbers){
+    return numbers.sort((a,b)=>a - b).slice(-2).reduce((a,b)=>a + b) 
+  }
