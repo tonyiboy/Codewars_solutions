cd0223750d40b97e23e0c9bc84c37defa9e27186
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-02 13:56:39 +0200

    Codewars_7kyu_solution_587.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_587.js
@@ -0,0 +1,5 @@
+// Find the number with the most digits.
+
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+let findLongest = array => array.reduce((a, b) => `${b}`.length > `${a}`.length ? b : a)
