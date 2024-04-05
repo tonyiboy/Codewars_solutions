e9b81e54a3747a32d6d8fee92821d398ba01ff71
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-05 20:50:13 +0200

    Codewars_7kyu_solution_616.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_616.js
@@ -0,0 +1,8 @@
+// Write a program that outputs the top n elements from a list.
+
+// Example:
+
+// largest(2, [7,6,5,4,3,2,1])
+// => [6,7]
+
+let largest = (n, array) => array.sort((a,b) => b - a).slice(0, n).reverse()
