bb321e57eec02716163ce8f7a1d99439f218457d
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-06 22:47:36 +0200

    Codewars_7kyu_solution_627.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_627.js
@@ -0,0 +1,21 @@
+// Write a function that converts any sentence into a V A P O R W A V E sentence. a V A P O R W A V E sentence converts all the letters into uppercase, 
+// and adds 2 spaces between each letter (or special character) to create this V A P O R W A V E effect.
+
+// Note that spaces should be ignored in this case.
+
+// Examples
+// "Lets go to the movies"       -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
+// "Why isn't my code working?"  -->  "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"
+
+function vaporcode(string) {
+    let arr=[]
+    let i = 0
+    do{ 
+      if (string[i]!=' '){ 
+        arr.push(string[i].toUpperCase())
+       }
+      i++
+    }while(i < string.length)
+      
+    return arr.join('  ');
+  }
