9cb0a3f56c9848e14e3b021b318108d36cbbef01
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-06 22:28:40 +0200

    Codewars_7kyu_solution_619.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_619.js
@@ -0,0 +1,7 @@
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, the returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+let last = x => x.split(" ").sort((a,b) => a.charCodeAt(a.length-  1) - b.charCodeAt(b.length - 1))
