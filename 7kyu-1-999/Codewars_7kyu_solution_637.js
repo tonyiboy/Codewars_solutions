618f227824c92d1e859eb3e0c13eb6981caba66c
tonyiboy <antoniocodinggithub@gmail.com>
2024-04-07 23:15:15 +0200

    Codewars_7kyu_solution_637.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_637.js
@@ -0,0 +1,8 @@
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+// Example:
+
+// 'acb' --> 'bca'
+// 'aabacbaa' --> 'bbabcabb'
+
+let switcheroo = x => x.split('').map(n => n === "a" ? "b" : n === "b" ? "a" : "c").join("")
