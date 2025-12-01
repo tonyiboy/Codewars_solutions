3330c5707a88ebc30079962d24bf2f527446fc18
tonyiboy <antoniocodinggithub@gmail.com>
2025-12-01 20:47:40 +0100

    Codewars_6kyu_solution_3.js

---
+++ 6kyu-1-999/Codewars_6kyu_solution_3.js
@@ -0,0 +1,29 @@
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in 
+// the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// Input = "The sunset sets at twelve o' clock."
+// Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
+
+function alphabetPosition(text) {
+ let lower = text.toLowerCase()
+  let result = ""
+  
+  for (let i = 0; i < lower.length; i++) {
+    if (lower[i].codePointAt(0) >= 97 && lower[i].codePointAt(0) <= 122 ) {
+        result += lower[i].codePointAt(0) -96 + " "  
+    } 
+  }
+
+  return result.slice(0,-1)
+}
+
+
+
