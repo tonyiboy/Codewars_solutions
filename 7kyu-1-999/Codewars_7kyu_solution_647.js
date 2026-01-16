373bf77b2bf929017679aad31aa907cbcea0b80e
tonyiboy <antoniocodinggithub@gmail.com>
2026-01-16 23:22:44 +0100

    Codewars_7kyu_solution_647.js

--- 7kyu-1-999/Codewars_7kyu_solution_647.js
+++ 7kyu-1-999/Codewars_7kyu_solution_647.js
@@ -4,7 +4,7 @@
 // and have to return the highest and lowest number.
 
 // Examples
-// highAndLow("1 2 3 4 5"); // return "5 1"
+// highAndLow("1 2 3 4 5"); // return "5 1" 
 // highAndLow("1 2 -3 4 5"); // return "5 -3"
 // highAndLow("1 9 3 4 -5"); // return "9 -5"
 // Notes
