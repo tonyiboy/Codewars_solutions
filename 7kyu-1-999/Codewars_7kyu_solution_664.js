37a34682d52fba59b7a10116183d3223d9990323
tonyiboy <antoniocodinggithub@gmail.com>
2026-01-20 16:35:22 +0100

    Codewars_7kyu_solution_664.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_664.js
@@ -0,0 +1,33 @@
+// Check to see if a string has the same amount of 'x's and 'o's. 
+// The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+function XO(str) {
+
+    let countX = 0
+    let countO = 0
+    let i = 0
+
+    do {
+
+        if (str[i] === "x" || str[i] === "X") {
+            countX++
+        } else if (str[i] === "o" || str[i] === "O") {
+            countO++
+        }
+        i++
+    } while (i < str.length)
+
+    if (countX === countO) {
+        return true
+    } else return false
+
+}
+
