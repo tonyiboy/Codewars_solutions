74d7a290f942e37d8bf79cb1f91ab1c300aee469
tonyiboy <antoniocodinggithub@gmail.com>
2026-01-22 21:46:44 +0100

    Codewars_7kyu_solution_682.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_682.js
@@ -0,0 +1,16 @@
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, 
+// you can be sure he's not...
+
+// Input = ["Ryan", "Kieran", "Jason", "Yous"]
+// Output = ["Ryan", "Yous"]
+
+// Input = ["Peter", "Stephen", "Joe"]
+// Output = []
+// Input strings will only contain letters.
+// Note: keep the original order of the names in the output.
+
+const friend = friends => friends.filter(n => n.length === 4)
+
+
