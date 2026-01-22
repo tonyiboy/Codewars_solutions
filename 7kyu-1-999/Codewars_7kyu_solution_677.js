2d8ac73205cb5306de6c4f1e6cddb28bdee5490d
tonyiboy <antoniocodinggithub@gmail.com>
2026-01-22 21:46:09 +0100

    Codewars_7kyu_solution_677.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_677.js
@@ -0,0 +1,25 @@
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, 
+// you can be sure he's not...
+
+// Input = ["Ryan", "Kieran", "Jason", "Yous"]
+// Output = ["Ryan", "Yous"]
+
+// Input = ["Peter", "Stephen", "Joe"]
+// Output = []
+// Input strings will only contain letters.
+// Note: keep the original order of the names in the output.
+
+function friend(friends){
+    let result = []
+
+  for (let i of friends) {
+    if (i.length === 4) {
+      result.push(i)
+    }
+  }
+
+  return result
+}
+console.log(friend(["Ryan", "Kieran", "Mark"]));
