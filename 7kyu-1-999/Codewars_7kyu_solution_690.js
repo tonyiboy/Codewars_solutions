6e65239e2d40fe5d17d450cf14738db1f9401118
tonyiboy <antoniocodinggithub@gmail.com>
2026-01-23 03:08:00 +0100

    Codewars_7kyu_solution_690.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_690.js
@@ -0,0 +1,17 @@
+// Complete the solution so that it returns true if the first argument(string) 
+// passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// Inputs: "abc", "bc"
+// Output: true
+
+// Inputs: "abc", "d"
+// Output: false
+
+function solution(str, ending){
+ 
+  
+  if (str.endsWith(ending)) return true
+  else return false
+}
