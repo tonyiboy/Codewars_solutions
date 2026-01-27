71d81e50d38c352ca363cd78ba06b3c80d8ee256
tonyiboy <antoniocodinggithub@gmail.com>
2026-01-27 18:52:54 +0100

    Codewars_7kyu_solution_712.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_712.js
@@ -0,0 +1,24 @@
+// An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+// Note: anagrams are case insensitive
+
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+// Examples
+// "foefet" is an anagram of "toffee"
+
+// "Buckethead" is an anagram of "DeathCubeK"
+
+// write the function isAnagram
+
+// write the function isAnagram
+var isAnagram = function(test, original) {
+  var t = test.toLowerCase().split('').sort().join('');
+  var o = original.toLowerCase().split('').sort().join('');
+  if (t === o) {
+    return true
+  } else {
+    return false
+  }
+  
+};
