e013f1ff9b5d2a5b550777ccc5bfd39f63e12609
tonyiboy <antoniocodinggithub@gmail.com>
2026-01-29 23:38:29 +0100

    Codewars_7kyu_solution_725.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_725.js
@@ -0,0 +1,17 @@
+// Create a function that returns the sum of the two lowest positive numbers 
+// given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+function sumTwoSmallestNumbers(numbers) {  
+  
+    let sorted = numbers.sort((a, b) => a - b)
+
+    return sorted[0] + sorted[1]
+    
+
+}
+
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]));
