10dba99c61346671d0288d7d976f5286d2306367
tonyiboy <antoniocodinggithub@gmail.com>
2026-01-30 01:47:06 +0100

    Codewars_7kyu_solution_740.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_740.js
@@ -0,0 +1,36 @@
+// Usually when you buy something, you're asked whether your credit card number, 
+// phone number or answer to your most secret question is still correct. However, 
+// since someone could look over your shoulder, you don't want that shown on your screen. 
+// Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// Examples (input --> output):
+// "4556364607935616" --> "############5616"
+//      "64607935616" -->      "#######5616"
+//                "1" -->                "1"
+//                 "" -->                 ""
+
+// // "What was the name of your first pet?"
+// "Skippy" --> "##ippy"
+// "Nananananananananananananananana Batman!" --> "####################################man!"
+
+// return masked string
+function maskify(cc) {
+
+  let number = cc.length - 5
+  let result = ""
+
+  for (let i = 0; i < cc.length; i++) {
+    if (i > number) {
+      result += cc[i]
+    } else {
+      result += "#"
+    }
+  }
+
+  return result
+  
+}
+
+console.log(maskify('Skippy'));
