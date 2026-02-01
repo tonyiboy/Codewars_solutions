864751b1bee1c09a8b838a724f13eee19cfe34ff
tonyiboy <antoniocodinggithub@gmail.com>
2026-02-01 23:37:36 +0100

    Codewars_7kyu_solution_748.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_748.js
@@ -0,0 +1,31 @@
+// ask:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+// Have fun!
+
+function oddOrEven(array) {
+      let result = 0
+     
+      array.forEach(item => result += item)
+      
+
+   if (result % 2 === 0) {
+     return "even"
+   } else {
+    return "odd"
+   }
+
+}
