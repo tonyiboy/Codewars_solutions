e54eccdd8deb2415b1e19a336ff7d2a8f541892e
tonyiboy <antoniocodinggithub@gmail.com>
2026-02-04 11:44:11 +0100

    Codewars_7kyu_solution_764.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_764.js
@@ -0,0 +1,31 @@
+// Write a function which takes a number as input and returns the sum of the absolute 
+// value of each of the number's decimal digits.
+
+// For example: (Input --> Output)
+
+// 10 --> 1
+// 99 --> 18
+// -32 --> 5
+// Let's assume that all numbers in the input will be integer values.
+
+function sumDigits(number) {
+  
+  
+    let newString = String(number)
+    let newArray = []
+    let i = 0
+
+    do {
+        
+        if (newString[i] === "-") {
+          i++
+            continue
+        } else {
+            newArray.push(Number(newString[i]))
+          i++
+        }
+        
+    } while (i < newString.length)
+
+    return newArray.reduce((acc, cur) => acc + cur)
+}
