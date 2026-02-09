fbb1422c039e5b98b1d47253e25a2275449839a6
tonyiboy <antoniocodinggithub@gmail.com>
2026-02-09 11:21:26 +0100

    Codewars_7kyu_solution_778.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_778.js
@@ -0,0 +1,33 @@
+// Task
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+function reverseLetter(str) {
+    let result = ""
+    let i = str.length-1
+
+    while ( i >= 0) {
+        if (str[i].codePointAt(0) >= 97 && str[i].codePointAt(0) <= 122) {
+            result += str[i]
+        }
+      i--
+               
+    }
+
+    return result
+  
+  
+}
+
+
+  
