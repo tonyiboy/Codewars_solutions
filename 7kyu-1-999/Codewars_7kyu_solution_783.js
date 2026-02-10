8b0d63ee36b83757a0b02e80e4515e9b9a6f6f76
tonyiboy <antoniocodinggithub@gmail.com>
2026-02-10 23:28:53 +0100

    Codewars_7kyu_solution_783.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_783.js
@@ -0,0 +1,33 @@
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, 
+// and return as shown below. Index 0 will be considered even.
+
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+// The input will be a lowercase string with no spaces.
+
+
+function capitalize(s){
+   let evenResult = []
+  let oddResult = []
+  let i = 0
+  
+  do {  
+    
+      if (s.length === 0) return ["",""]
+    
+      if (i % 2 === 0) {
+          evenResult.push(s[i].toUpperCase())
+        } else {
+            evenResult.push(s[i].toLowerCase())
+    }
+
+    if (i % 2 !== 0) {
+        oddResult.push(s[i].toUpperCase())
+    } else {
+        oddResult.push(s[i].toLowerCase())
+    }  
+    i++
+} while (i < s.length)
+
+  return [evenResult.join(""), oddResult.join("")]
+};
