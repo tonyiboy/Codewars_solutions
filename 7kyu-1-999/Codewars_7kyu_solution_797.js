8da6c85458084b1f0f7c9a869aac2fcc668f93ad
tonyiboy <antoniocodinggithub@gmail.com>
2026-02-11 14:20:01 +0100

    Codewars_7kyu_solution_797.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_797.js
@@ -0,0 +1,32 @@
+// In this Kata, you will be given a string that may have mixed uppercase and 
+// lowercase letters and your task is to convert that string to either lowercase 
+// only or uppercase only based on:
+
+// make as few changes as possible.
+// if the string contains equal number of uppercase and lowercase letters, 
+// convert the string to lowercase.
+// For example:
+
+// solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+// solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+// solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+
+function solve(s){
+    let lowerCaseCount = 0
+    let upperCaseCount = 0
+    let i = 0; 
+
+    do {
+        if (s[i] === s[i].toLowerCase()) {
+            lowerCaseCount++
+        } else if (s[i] === s[i].toUpperCase()) {
+            upperCaseCount++
+        }
+      i++
+    } while (i < s.length)
+
+    return lowerCaseCount >= upperCaseCount ? s.toLowerCase() : s.toUpperCase()
+  
+}
+
+
