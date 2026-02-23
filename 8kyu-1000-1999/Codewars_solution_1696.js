80a53ce4d82d01964aa8df3aeb54a43b35e02f45
tonyiboy <antoniocodinggithub@gmail.com>
2026-02-23 20:06:48 +0100

    Codewars_solution_1696.js

--- 8kyu-1000-1999/Codewars_solution_1696.js
+++ 8kyu-1000-1999/Codewars_solution_1696.js
@@ -1,7 +1,7 @@
 // You task is to implement an simple interpreter for the notorious esoteric language HQ9+ that 
 // will work for a single character input:
 
-// If the input is 'H', return 'Hello World!'
+// If the input is 'H', return 'Hello World!'   
 // If the input is 'Q', return the input
 // If the input is '9', return the full lyrics of 99 Bottles of Beer. It should be formatted like this:
 // 99 bottles of beer on the wall, 99 bottles of beer.
