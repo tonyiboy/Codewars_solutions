3d757428141dfe63c3cdd94cfa911327d72ef8de
tonyiboy <antoniocodinggithub@gmail.com>
2026-02-24 23:06:14 +0100

    Codewars_solution_1703.js

---
+++ 8kyu-1000-1999/Codewars_solution_1703.js
@@ -0,0 +1,41 @@
+// Description:
+// Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. 
+// For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+
+// Examples
+// "Hi!"     ---> "Hi!"
+// "Hi!!!"   ---> "Hi!"
+// "!Hi"     ---> "Hi!"
+// "!Hi!"    ---> "Hi!"
+// "Hi! Hi!" ---> "Hi Hi!"
+// "Hi"      ---> "Hi!"
+
+// function remove (string) {
+//     let result = ""
+//     let i = 0
+
+//     do {
+//         if (string[i] !== "!") {
+//             result += string[i]
+//         }
+//       i++
+//     } while (i < string.length)
+
+//     return result + "!"
+// } 
+
+
+function remove (string) {
+    let result = ""
+
+    for (let i of string) {
+        if (i !== "!") {
+            result += i
+        }
+    }
+
+    return result + "!"
+} 
+
+
+
