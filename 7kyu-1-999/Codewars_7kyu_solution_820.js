a3e02d815a85e5a3052718fe5f6b2a49bee0d41d
tonyiboy <antoniocodinggithub@gmail.com>
2026-03-10 23:18:09 +0100

    Codewars_7kyu_solution_820.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_820.js
@@ -0,0 +1,13 @@
+// In this kata, you need to write a function that takes a string and a letter as 
+// input and then returns the index of the second occurrence of that letter in the string. 
+// If there is no such letter in the string, or if the letter occurs only once in the string, -1 should be returned.
+
+// Examples:
+
+// for inputs  "Hello world!!!", 'l'  ->  return 3
+// for inputs  "Hello world!!!", 'A'  ->  return -1
+// Good luck ;) And don't forget to rate this kata if you liked it.
+
+function secondSymbol(s, symbol) {
+  return s.indexOf(symbol, s.indexOf(symbol) + 1);
+}
