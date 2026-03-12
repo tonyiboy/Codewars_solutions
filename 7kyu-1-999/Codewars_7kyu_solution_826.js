88baea8712904134456f3883980e5625c6372b15
tonyiboy <antoniocodinggithub@gmail.com>
2026-03-12 23:06:46 +0100

    Codewars_7kyu_solution_826.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_826.js
@@ -0,0 +1,30 @@
+// I will give you two strings. I want you to transform stringOne into stringTwo one letter at a time.
+
+// Example:
+
+// stringOne = 'bubble gum';
+// stringTwo = 'turtle ham';
+
+// Result:
+// bubble gum
+// tubble gum
+// turble gum
+// turtle gum
+// turtle hum
+// turtle ham
+
+function mutateMyStrings(stringOne, stringTwo){
+  let result = '';
+  let current = stringOne.split('');
+  
+  result += current.join('') + '\n';
+  
+  current.forEach((char, index) => {
+    if (char !== stringTwo[index]) {
+      current[index] = stringTwo[index];
+      result += current.join('') + '\n';
+    }
+  });
+  
+  return result;
+}
