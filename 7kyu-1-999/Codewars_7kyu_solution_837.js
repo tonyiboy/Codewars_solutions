4e17b547584252a5e12428c3556b2e4dfa5d5cff
tonyiboy <antoniocodinggithub@gmail.com>
2026-03-29 23:31:01 +0200

    Codewars_7kyu_solution_837.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_837.js
@@ -0,0 +1,18 @@
+// Find the number with the most digits.
+
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+
+function findLongest(array){
+  
+let num = array[0] 
+  
+  for (let i = 1; i < array.length; i++) {
+
+    if (String(num).length < String(array[i]).length ) num = array[i]
+    
+  }
+
+ return num
+  
+}
