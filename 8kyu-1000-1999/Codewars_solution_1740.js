35df557c933cfb0fdd6df3518866a0fb9bb4357b
tonyiboy <antoniocodinggithub@gmail.com>
2026-04-26 22:19:49 +0200

    Codewars_solution_1740.js

---
+++ 8kyu-1000-1999/Codewars_solution_1740.js
@@ -0,0 +1,7 @@
+// Complete the function that calculates the area of the red square, 
+// when the length of the circular arc A is given as the input.
+
+// A
+// Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+
+const squareArea = A => (2 * A / Math.PI) ** 2
