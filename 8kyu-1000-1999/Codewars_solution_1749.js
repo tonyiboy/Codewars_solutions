7261b9a93ea943aec80aa040d918711bb5d4ad3b
tonyiboy <antoniocodinggithub@gmail.com>
2026-04-30 00:57:48 +0200

    Codewars_solution_1749.js

---
+++ 8kyu-1000-1999/Codewars_solution_1749.js
@@ -0,0 +1,32 @@
+// Task
+// Create a method all which takes two params:
+
+// a sequence
+// a function
+// and returns true if the function in the params returns true for every element in the sequence. 
+// Otherwise, it should return false. If the sequence is empty, it should return true, 
+// since technically nothing failed the test.
+
+// Example
+// all((1, 2, 3, 4, 5), greater_than_9) -> false
+// all((1, 2, 3, 4, 5), less_than_9)    -> True
+
+
+function all(arr, fun) {
+  
+  if (arr.length === 0) return true
+  
+  let i = 0
+  
+ do {
+      
+      if(!fun(arr[i])) {
+        return false 
+      }
+   
+    i++
+      
+    } while (i < arr.length) 
+
+    return true
+}
