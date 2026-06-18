e7579d5a65711983e400db7a0782b45458df8b93
tonyiboy <antoniocodinggithub@gmail.com>
2026-06-18 22:31:47 +0200

    Codewars_7kyu_solution_851.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_851.js
@@ -0,0 +1,36 @@
+// Generate HTML links
+// We need a HTML menu.... but writing HTML over and-over-again is boooring... 
+// Let's just generate it instead!
+
+// Task:
+// Write a function generateMenu() with the following inputs/output:
+
+// Inputs:
+
+// menuItems: An array of objects (with url and text properties), which represents our menu items
+
+// Output:
+
+// A string of HTML containing an anchor tag for each element of menuItems (with 
+// the appropriate href attribute and text content)
+
+function generateMenu (menuItems) {
+  
+    if (menuItems.length === 0) return ""
+    
+    let html = ""
+    let i = 0
+    
+    do {
+      html += `<a href="${menuItems[i].url}">${menuItems[i].text}</a>`
+      i++
+    } while (i < menuItems.length)
+    
+    
+    return html
+}
+
+
+
+
+
