4e2168751ceec4a50ea30571107a5778cebf7704
tonyiboy <antoniocodinggithub@gmail.com>
2026-06-29 22:14:21 +0200

    Codewars_7kyu_solution_853.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_853.js
@@ -0,0 +1,35 @@
+// You have to create a function which receives 3 number arguments: 2 operands a and b, and the result of an unknown operation performed on them.
+
+// Based on those 3 values you have to return a string, that describes which operation was used to get the given result.
+
+// The possible return strings are: "addition", "subtraction", "multiplication", "division".
+
+// Examples:
+// (a = 1, b = 2, result = 3)   --> 1 ? 2 = 3   --> "addition"
+// (a = 5, b = 2, result = 2.5) --> 5 ? 2 = 2.5 --> "division"
+// Notes
+// The / operator performs a plain division without rounding.
+// You can assume that there will always be a unique valid answer 
+// (no ambiguous cases like e.g. 1 ? 0 = 0 which could be either - or +, or 3 ? 1 = 3 
+// which could be either * or /).
+// You can assume that there will be no division by 0
+
+function calcType(a, b, res) {
+  
+
+    if (a + b === res) {
+        return "addition"
+    } else if (a - b === res) {
+        return "subtraction"
+    } else if (a * b === res) {
+        return "multiplication"
+    } else {
+        return "division"
+    }
+}
+
+console.log(calcType(1, 2, 3));
+
+
+
+
