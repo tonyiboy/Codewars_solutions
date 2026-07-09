2b0b8df59499ef4dae9715465f21fa08d6c505ad
tonyiboy <antoniocodinggithub@gmail.com>
2026-07-09 23:17:56 +0200

    Codewars_7kyu_solution_870.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_870.js
@@ -0,0 +1,34 @@
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b 
+// (change a to b and vice versa). Leave any incidence of c untouched.
+
+// Example:
+
+// 'acb' --> 'bca'
+// 'aabacbaa' --> 'bbabcabb'
+
+function switcheroo(x){
+
+  let result = ""
+  let i = 0
+
+  do {
+    
+    switch(true) {
+        case x[i] === "a":
+          result += "b";
+        break;
+        case x[i] === "b":
+           result += "a";
+        break;
+        default:
+          result+= "c";
+        break;
+    }
+    
+    i++
+  
+  } while (i < x.length)
+
+  return result
+  
+}
