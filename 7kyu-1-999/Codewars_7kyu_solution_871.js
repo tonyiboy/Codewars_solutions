237b687b23f5a50dd1fda1d8a3d1e359ebbcdb50
tonyiboy <antoniocodinggithub@gmail.com>
2026-07-09 23:18:02 +0200

    Codewars_7kyu_solution_871.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_871.js
@@ -0,0 +1,23 @@
+// Filter the number
+// Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, 
+// can you return the number back to its original state?
+
+// Task
+// Your task is to return a number from a string.
+
+// Details
+// You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string 
+// in the order they occur.
+
+function filterString(value) {
+  let result = ""
+
+  for (let i = 0; i < value.length; i++) {
+      if (Number(value[i]) || Number(value[i]) === 0) {
+        result += value[i]
+      }
+  }
+
+
+  return Number(result)
+}
