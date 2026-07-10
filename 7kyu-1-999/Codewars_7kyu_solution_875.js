4c068e09d8ea7f15bd0a3a06c5562b3d1c529589
tonyiboy <antoniocodinggithub@gmail.com>
2026-07-10 02:44:49 +0200

    Codewars_7kyu_solution_875.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_875.js
@@ -0,0 +1,39 @@
+// input:
+
+// a string strng
+// an array of strings arr
+// Output of function contain_all_rots(strng, arr) (or containAllRots or contain-all-rots):
+
+// a boolean true if all rotations of strng are included in arr
+// false otherwise
+// Examples:
+// contain_all_rots(
+//   "bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]) -> true
+
+// contain_all_rots(
+//   "Ajylvpy", ["Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee"]) -> false)
+// Note:
+// Though not correct in a mathematical sense
+
+// we will consider that there are no rotations of strng == ""
+// and for any array arr: contain_all_rots("", arr) --> true
+
+
+function containAllRots(strng, arr) {
+  if (strng === "") return true;
+
+  let rotation = strng;
+  let i = 0
+
+  while (i < strng.length) {
+    if (!arr.includes(rotation)) {
+      return false;
+    }
+
+    rotation = rotation.slice(1) + rotation[0];
+    i++
+  }
+
+  return true;
+}
+
