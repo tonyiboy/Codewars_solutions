7fd1df192d139b04bf58f7cde8c3ddfd7827c35f
tonyiboy <antoniocodinggithub@gmail.com>
2026-07-12 01:01:29 +0200

    Codewars_7kyu_solution_880.js

---
+++ 7kyu-1-999/Codewars_7kyu_solution_880.js
@@ -0,0 +1,26 @@
+// Given an array of numbers (in string format), 
+// you must return a string. The numbers correspond to the letters of the alphabet in reverse order: 
+// a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+
+// All inputs will be valid.
+
+function switcher(x){
+
+  
+     let letters = [" ", "z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c","b","a","!","?", " "]
+
+    let result = ""
+
+
+    for (let i in x) {
+        result += letters[x[i]]
+    }
+
+
+
+
+
+    return result
+}
+
+
